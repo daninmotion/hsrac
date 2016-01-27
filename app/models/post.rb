1a3afbabde701a3558104afeb4d410eb11b8a55e
daninmotion <dan85.cardenas@gmail.com>
2016-01-26 21:16:50 -0500

    News index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ActiveRecord::Base
-	validates :title, presence: true
 	has_attached_file :image, styles: { large: "600x600>", medium: "300x300", thumb: "150x150"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	validates :body, presence: true
