51269fb0c785769e47441cd2adfad09c79ac95f1
Daniel Cardenas <dan85.cardenas@gmail.com>
2016-02-05 16:12:05 -0500

    Linked Member Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    posts_path(current_user)
+    root_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
-    posts_path(current_user)
+    root_path(current_user)
   end
 end
